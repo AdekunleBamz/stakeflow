3559a6506d209c0c779ca79cadbb7049a03ce683
AdekunleBamz <adekunle@example.com>
2026-01-20 10:18:09 +0100

    Add hooks barrel export file

---
+++ frontend/src/hooks/index.ts
@@ -0,0 +1,6 @@
+export { useNFTs } from "./useNFTs";
+export { useStakingStats } from "./useStakingStats";
+export { useBlockHeight } from "./useBlockHeight";
+export { usePendingRewards } from "./usePendingRewards";
+export { useToast } from "./useToast";
+export { useContractCall } from "./useContractCall";
