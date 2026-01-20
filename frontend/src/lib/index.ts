4e22666d1640d7d1f734bd0cb6243821ed240d21
AdekunleBamz <adekunle@example.com>
2026-01-20 10:49:18 +0100

    Add lib barrel export

---
+++ frontend/src/lib/index.ts
@@ -0,0 +1,19 @@
+/**
+ * Lib barrel export
+ */
+
+// API functions
+export * from "./api";
+
+// Constants
+export * from "./constants";
+
+// Types
+export * from "./types";
+
+// Utilities
+export * from "./formatters";
+export * from "./validators";
+export * from "./contracts";
+export * from "./storage";
+export * from "./errors";
