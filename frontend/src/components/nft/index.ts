09dbf1ce6ed83ccc7c75b271897db140a8722f2a
AdekunleBamz <adekunle@example.com>
2026-01-20 12:02:56 +0100

    Add NFT components barrel export

---
+++ frontend/src/components/nft/index.ts
@@ -0,0 +1,4 @@
+export { NFTDetail, NFTAttributes, NFTStats } from './NFTDetail';
+export { NFTFilter, NFTBatchSelect } from './NFTFilter';
+export { NFTPreview, NFTStack, NFTMiniCard, NFTList } from './NFTPreview';
+export { NFTSorter, NFTSearch, NFTManager } from './NFTManager';
