a9247805f405faaae36405d25a504ff84d0bb806
AdekunleBamz <adekunle@example.com>
2026-01-20 11:52:37 +0100

    Add common components barrel export

---
+++ frontend/src/components/common/index.ts
@@ -0,0 +1,3 @@
+export { Breadcrumbs, PageHeader, EmptyState, StatCard, Tabs } from './PageElements';
+export { NFTCard, NFTGrid } from './NFTCard';
+export { TokenDisplay, BalanceCard, WalletBalances } from './TokenDisplay';
