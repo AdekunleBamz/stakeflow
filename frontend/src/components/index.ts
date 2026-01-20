7c8ce84a4fa01fc4d02f94b720d3590c7937bea8
AdekunleBamz <adekunle@example.com>
2026-01-20 10:38:28 +0100

    Add components barrel export

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,23 @@
+// Components barrel export
+export { default as MintSection } from "./MintSection";
+export { default as StakingDashboard } from "./StakingDashboard";
+export { default as RewardsPanel } from "./RewardsPanel";
+export { default as Navbar } from "./Navbar";
+
+// Enhanced components
+export { NavbarEnhanced } from "./NavbarEnhanced";
+export { MintSectionEnhanced } from "./MintSectionEnhanced";
+export { StakingDashboardEnhanced } from "./StakingDashboardEnhanced";
+export { RewardsPanelEnhanced } from "./RewardsPanelEnhanced";
+
+// Feature components
+export { NFTCard } from "./NFTCard";
+export { NFTGrid } from "./NFTGrid";
+export { StatsGrid, SingleStat } from "./StatsGrid";
+export { TransactionHistory } from "./TransactionHistory";
+export { WalletInfo } from "./WalletInfo";
+export { BlockCounter, NetworkStatus } from "./BlockCounter";
+export { RewardCalculator } from "./RewardCalculator";
+export { EmptyState, NoNFTsState, NoStakedNFTsState, WalletNotConnectedState } from "./EmptyState";
+export { FeatureCard, FeaturesSection } from "./FeaturesSection";
+export { Footer } from "./Footer";
